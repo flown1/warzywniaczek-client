f7280e6267592049484eb99a160d398c3abdd4c8
Wiktor <tamtennormalny@gmail.com>
2018-03-03 21:02:16 +0100

    Navbar and product update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,10 +4,11 @@ import '../Styles/Navbar.css';
 const Navbar = () => {
     return(
         <nav id="top-menu">
-            <a href="#" class="top-menu-button"><h3>Shopping</h3></a>
             <header className="App-header">
                 <h1 className="App-title">Grocery Store</h1>
+                <div class='side-box'></div>
             </header>
+            <a href="#" class="top-menu-button"><h3>Shopping</h3></a>
             <a href="#" class="top-menu-button"><h3>About us</h3></a>
         </nav>
     )
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -4,10 +4,15 @@ import '../Styles/Product.css';
 const Product = ({name, price, imgUrl}) => {
     return(
         <div class="product-box">
-            <img src={imgUrl} class="product-picture" />
-            <h2 class='product-name'>{name}</h2>
-            <h3 class='product-price'>{price}</h3> 
-            <button class="add-to-cart-btn">Buy</button>
+            <div class="box-top">
+                <img src={imgUrl} class="product-picture" />
+                <h2 class='product-name'>{name}</h2>
+            </div>
+            <hr/>
+            <div class="product-bottom">
+                <h3 class='product-price'>${price}</h3> 
+                <div class="add-to-cart-btn">Buy</div>
+            </div>
         </div>
     )
 }
