e6f085ed5a7d5c7b2ed2e3eae45c95dc0d8d0fab
Wiktor <tamtennormalny@gmail.com>
2018-03-03 21:17:06 +0100

    Added sidebox to header

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav id="top-menu">
             <header className="App-header">
                 <h1 className="App-title">Grocery Store</h1>
-                <div class='side-box'></div>
+                <div class='side-box'>Do Grocery<br/>Online</div>
             </header>
             <a href="#" class="top-menu-button"><h3>Shopping</h3></a>
             <a href="#" class="top-menu-button"><h3>About us</h3></a>
